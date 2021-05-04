82a7188add874536893ea020efcc205a1cccf35b
Truong Dao <ton.truong.dao@outlook.com>
2021-05-04 10:05:27 -0400

    Initiated Components varables & States

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 const App = () => {
+  // ##### Component Variables
+  var status = {
+    wip: [],
+    complete: []
+  }
+
+  // ##### React States
+  const [tasks, setTasks] = useState([{
+    name:"Learn Angular",             
+    category:"wip",              
+    bgcolor: "salmon"},
+    {
+    name:"React",              
+    category:"wip",              
+    bgcolor:"lightblue"},
+    {
+    name:"Vue",              
+    category:"complete",
+    bgcolor:"lightgreen"  
+  }])
+
+
+
   return (
     <div className='container-kanban'>
       KANBAN DEMO
